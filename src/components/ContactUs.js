ac1846320072d504843abddf01fcd7e6111e63d6
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-09-16 14:45:35 +0300

    final commit

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,10 +1,11 @@
 
 // src/components/ContactUs.js
 import React from 'react';
+import "./contacts.css";
 
 function ContactUs() {
     return(
-        <div>
+        <div className="contact-container">
             <p><strong>Email</strong>pulsenet@gmail.com</p>
             <p><strong>Phone number</strong>0798657421</p>
             <p>You can also visit our branch at Meta road in Imagine City</p>
